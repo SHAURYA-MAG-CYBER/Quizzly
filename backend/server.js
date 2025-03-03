ec36a48d78ff441ac59408e9d31ae01710bd068d
shauryaAgr <shaurya9862@gmail.com>
2025-03-04 02:37:22 +0530

    Railway deployment

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,16 @@ app.use(express.json());
 app.use("/api/quiz", quizRoutes);
 app.use("/api/attempt", attemptRoutes);
 
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
 mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("MongoDB connection Error: ", err));
 
 app.get("/", (req,res) => {
-    res.send("Quizzly Backend is running");
+    res.send("Backend is running on Railway");
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen (PORT, () => console.log(`Server running on port ${PORT}`));
